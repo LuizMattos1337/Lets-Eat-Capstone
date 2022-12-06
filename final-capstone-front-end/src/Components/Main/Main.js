ea8bf904d075faa38d79a9fe00dc6fa148b2e890
Luiz M <luizmattos1337@gmail.com>
2022-12-05 18:52:09 -0600

    minor: clean up

--- final-capstone-front-end/src/Components/Main/Main.js
+++ final-capstone-front-end/src/Components/Main/Main.js
@@ -1,16 +1,16 @@
-import React from "react";
-import Login from "../Login/Login";
-import Register from "../Register/Register";
-import Navbar from "../Navbar/Navbar";
-import Home from "../Home/Home";
-import EventView from "../EventView/EventView";
-import { Route, Routes } from "react-router-dom";
-import { useUser } from "../UserProvider/UserProvider";
-import { Navigate } from "react-router";
-import MyEvents from "../MyEvents/MyEvents";
+import React from 'react';
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+import Navbar from '../Navbar/Navbar';
+import Home from '../Home/Home';
+import EventView from '../EventView/EventView';
+import { Route, Routes } from 'react-router-dom';
+import { useUser } from '../UserProvider/UserProvider';
+import { Navigate } from 'react-router';
+import MyEvents from '../MyEvents/MyEvents';
 
-import Restaurants from "../Restaurants/Restaurants";
-import NewEvent from "../NewEvent/NewEvent";
+import Restaurants from '../Restaurants/Restaurants';
+import NewEvent from '../NewEvent/NewEvent';
 export default function Main() {
 	const user = useUser();
 
@@ -19,75 +19,65 @@ export default function Main() {
 			<Navbar />
 			<Routes>
 				<Route
-					path="/home"
+					path='/home'
 					element={
 						user.token !== undefined ? (
 							<MyEvents />
 						) : (
-							<Navigate to="/login" replace />
+							<Navigate to='/login' replace />
 						)
 					}
 				/>
 
 				<Route
-					path="/home"
+					path='/home'
 					element={
 						user.token !== undefined ? (
 							<Home />
 						) : (
-							<Navigate to="/login" replace />
+							<Navigate to='/login' replace />
 						)
 					}
 				/>
-				
+
 				<Route
-					path="/newEvent"
+					path='/newEvent'
 					element={
 						user.token !== undefined ? (
 							<NewEvent />
 						) : (
-							<Navigate to="/login" replace />
+							<Navigate to='/login' replace />
 						)
 					}
 				/>
 				<Route
-					path="/eventView/:eventViewId"
+					path='/eventView/:eventViewId'
 					element={
 						user.token !== undefined ? (
 							<EventView />
 						) : (
-							<Navigate to="/login" replace />
+							<Navigate to='/login' replace />
 						)
 					}
 				/>
-				<Route path="/vote/:eventViewId/:guestLink" element={<EventView/>}/>
+				<Route path='/vote/:eventViewId/:guestLink' element={<EventView />} />
 
 				<Route
-					path="/login"
-					element={
-						!user.token ? (
-							<Login />
-						) : (
-							<Navigate to="/home" replace />
-						)
-					}
+					path='/login'
+					element={!user.token ? <Login /> : <Navigate to='/home' replace />}
 				/>
-				<Route path="/Restaurants" element={<Restaurants />} />
-				<Route path="/register" element={<Register />} />
-				<Route path="*" element={<Navigate to="/home" replace />} />
+				<Route path='/Restaurants' element={<Restaurants />} />
+				<Route path='/register' element={<Register />} />
+				<Route path='*' element={<Navigate to='/home' replace />} />
 			</Routes>
 			{/* This is a debug menu for the current Context */}
-			{/* Delete this before final */}
-			<aside className="dev-mode">
+			<aside className='dev-mode'>
 				<small>
 					<hr />
-					Userid is{" "}
-					<strong>{user.id === null ? "null" : "not null"}</strong>
+					Userid is <strong>{user.id === null ? 'null' : 'not null'}</strong>
 					<br />
-					Token is{" "}
-					<strong>
-						{user.token === undefined ? "undefined" : "defined"}
-					</strong>
+					Token is{' '}
+					<strong>{user.token === undefined ? 'undefined' : 'defined'}</strong>
 				</small>
 			</aside>
 		</>
